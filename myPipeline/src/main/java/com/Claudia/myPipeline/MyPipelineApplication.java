0475f91786617efa5cf6ce6e408870eb558de4c2
Ubuntu <ubuntu@ip-172-31-16-240.ec2.internal>
2024-05-26 01:56:16 +0000

    Add folder

---
+++ myPipeline/src/main/java/com/Claudia/myPipeline/MyPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.Claudia.myPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPipelineApplication.class, args);
+	}
+
+}
---
+++ myPipeline/src/test/java/com/Claudia/myPipeline/MyPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Claudia.myPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
